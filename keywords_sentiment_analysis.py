46e91313b1b3c41180a69a6b7f732a73066a2e22
James Thomas Widjaja <48656293+jamesthomasw@users.noreply.github.com>
2021-09-30 20:28:37 +0700

    Sentiment scoring for live tweets

--- keywords_sentiment_analysis.py
+++ keywords_sentiment_analysis.py
@@ -9,21 +9,21 @@ from developer_api_key import consumer_key, consumer_secret
 auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
 api = tweepy.API(auth)
 
-def get_tweets(keyword: str) -> List[str]:
+def get_tweets(keyword):
     all_tweets = []
     for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang="en").items(50):
         all_tweets.append(tweet.full_text)
 
     return all_tweets
 
-def clean_tweets(all_tweets: List[str]) -> List[str]:
+def clean_tweets(all_tweets):
     clean_tweets = []
     for tweet in all_tweets:
         clean_tweets.append(p.clean(tweet))
 
     return clean_tweets
 
-def calc_sentiment_score(clean_tweets: List[str]) -> List[float]:
+def calc_sentiment_score(clean_tweets):
     sentiment_score = []
     for tweet in clean_tweets:
         blob = TextBlob(tweet)
@@ -31,7 +31,7 @@ def calc_sentiment_score(clean_tweets: List[str]) -> List[float]:
 
     return sentiment_score
 
-def calc_avg_sentiment_score(keyword: str) -> int:
+def calc_avg_sentiment_score(keyword):
     tweets = get_tweets(keyword)
     tweets_clean = clean_tweets(tweets)
     sentiment_score = calc_sentiment_score(tweets_clean)
@@ -40,17 +40,16 @@ def calc_avg_sentiment_score(keyword: str) -> int:
 
     return avg_score
 
-if __name__  == "__main__":
-    print("Which do most people around the world prefer?")
-    first_keyword = input()
-    print("...or...")
-    second_keyword = input()
-    print("\n")
+print("Which do most people around the world prefer?")
+first_keyword = input()
+print("...or...")
+second_keyword = input()
+print("\n")
 
-    first_score = calc_avg_sentiment_score(first_keyword)
-    second_score = calc_avg_sentiment_score(second_keyword)
+first_score = calc_avg_sentiment_score(first_keyword)
+second_score = calc_avg_sentiment_score(second_keyword)
 
-    if (first_score > second_score):
-        print(f'{first_keyword} is more preferable than {second_keyword}')
-    else:
-        print(f'{second_keyword} is more preferable than {first_keyword}')
+if (first_score > second_score):
+    print(f'{first_keyword} is more preferable than {second_keyword}')
+else:
+    print(f'{second_keyword} is more preferable than {first_keyword}')
